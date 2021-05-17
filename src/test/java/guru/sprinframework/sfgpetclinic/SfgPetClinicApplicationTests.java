70eab7fa07360b12b6e925f91189cec705c34b7f
EvgenyN <jeka.nikitchenkooo@gmail.com>
2021-05-17 22:24:14 +0300

    Initial project add

---
+++ src/test/java/guru/sprinframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.sprinframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
