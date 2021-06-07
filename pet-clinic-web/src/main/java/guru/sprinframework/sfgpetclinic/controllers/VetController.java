f8a10ea943ac5bb90c5714c28b5366396a42e082
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-06-07 22:28:56 +0300

    adding list of vets. Close #15

--- pet-clinic-web/src/main/java/guru/sprinframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/sprinframework/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,22 @@
 package guru.sprinframework.sfgpetclinic.controllers;
 
+import guru.sprinframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
 
-  @RequestMapping({"/vets","/vets/vetIndex","vets/vetIndex.html" })
-public String listVets (){
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","/vets/vetIndex","vets/vetIndex.html" })
+public String listVets (Model model) {
+        model.addAttribute("vets", vetService.findAll());
     return "vets/vetIndex";
 }
 
